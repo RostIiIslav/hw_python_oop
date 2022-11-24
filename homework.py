bb8dc029c2f0a027582a880ccc195f0c3986d543
Рыманов Ростислав <rostiislav23@yandex.ru>
2022-11-24 11:25:26 +0300

    andnow

--- homework.py
+++ homework.py
@@ -178,5 +178,5 @@ if __name__ == "__main__":
     ]
 
     for workout_type, data in packages:
-        training: list[str, List[int]] = read_package(workout_type, data)
+        training: list[str, list[int]] = read_package(workout_type, data)
         main(training)
